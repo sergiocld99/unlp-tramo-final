1dc5480f10ba2e94c4d70483cac3a2417c45b721
linux <cs10.sergiocarp@gmail.com>
2022-04-07 09:51:16 -0300

    A

---
+++ BD/p1/adicionales/p1-ej12-fisico.java
@@ -0,0 +1,24 @@
+// COMILLAS = SUBRAYADO
+
+/* Jerarquia de Persona */
+PERSONA("dni", apellido, nombre)
+PACIENTE("dni(fk)")
+MEDICO("dni(fk)", matricula)
+
+/* Persona - Teléfono */
+TELEFONO("telefono")
+CONTACTO("dni(fk), telefono(fk)")
+
+/* Paciente - Médico */
+MED_CABECERA("dniPaciente(fk)", dniMedico(fk))
+
+/* Médico - Especialización */
+ESPECIALIZACION("nombre")
+TIENE("dni(fk), nombre(fk)")
+
+/* Médico - CLinica */
+CLINICA("razon_social", calle, nro, piso?, dpto?)
+TRABAJA("dni(fk), razon_social(fk), fecha_inicio", fecha_fin?)
+
+// Comentario: fecha_inicio de trabaja debe pertenecer a la clave primaria
+// porque puede ocurrir que un médico vuelva a trabajar en una clinica en otra ocasión
