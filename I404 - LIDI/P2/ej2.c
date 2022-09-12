0dbb70853628b8d581584d497abcebf7da157779
Sergio Calderón <66924320+sergiocarp10@users.noreply.github.com>
2022-09-12 03:16:00 -0300

    ej 2 (sin compilar)

---
+++ I404 - LIDI/P2/ej2.c
@@ -0,0 +1,119 @@
+// Dado un vector de N elementos, contar las ocurrencias de un numero dado
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Librería P-threads
+#include <pthread.h>
+
+// Cabecera para dwall time
+#include <sys/time.h>
+
+// ******************************* DWALL TIME *********************** //
+
+//Para calcular tiempo
+double dwalltime(){
+        double sec;
+        struct timeval tv;
+
+        gettimeofday(&tv,NULL);
+        sec = tv.tv_sec + tv.tv_usec/1000000.0;
+        return sec;
+}
+
+// ----------------------------------------------------------------------
+
+// variables compartidas
+static int *v;
+static const int X = 10;
+static int ocurrencias = 0;
+static pthread_mutex_t mutex;
+
+// estructura de params
+struct {
+    int startIndex;
+    int querySize;
+} pthread_params_t;
+
+// Prototipos de funcion
+void* resolver(void* arg);
+static void init_vector(int* v, int N, int max);
+
+int main(int argc,char*argv[]){
+
+    // variables
+    int N, T, i;
+    double timetick;
+
+    // lectura de argumentos
+    if ((argc != 3) || ((N = atoi(argv[1])) <= 0) || ((T = atoi(argv[2])) <= 0) ){
+        printf("\nError: hay que especificar N y T (tamaño del vector y cantidad de hilos)\n");
+        exit(1);
+    }
+
+    // reservar memoria
+    v = (int*) malloc(sizeof(int)*N);
+
+    // rellenar vector
+    init_vector(v, N, 2*X);
+
+    // variables para los threads
+    pthread_attr_t attr;
+    pthread_t threads[T];
+    pthread_params_t params[T];
+
+    // inicializar mutex
+    pthread_mutex_init(&mutex, NULL);
+
+    // marcar inicio de tiempo
+    timetick = dwalltime();
+
+    // crear hilos
+    for (i=0; i<N; i++){
+        params[i].startIndex = i*(N/T);
+        params[i].querySize = N/T;
+
+        pthread_create(&threads[i], &attr, resolver, &params[i]);
+    }
+
+    // Esperamos a que termine cada hilo
+    for (i=0; i<T; i++){
+        pthread_join(threads[i], NULL);
+    }
+
+    // marcamos fin de tiempo
+    printf("Ocurrrencias de %d en vector: %d. Tiempo en segundos %f\n",X,ocurrencias, dwalltime() - timetick);
+
+    return 0;
+}
+
+static void init_vector(int* v, int N, int max){
+    int i;
+
+    // nueva seed
+    srand(time(NULL));
+
+    for (i=0; i<N; i++){
+        // asignar numero entre 0 y 2X
+        v[i] = rand() % max;
+    }
+}
+
+void* resolver(void* arg){
+
+    int i, ocurrenciasLocal = 0;
+    pthread_params_t params = (pthread_params_t*) arg;
+    int startIndex = (*params).startIndex;
+    int querySize = (*params).querySize;
+
+    for (i=startIndex; i<querySize, i++){
+        if (v[i] == X) ocurrenciasLocal++;
+    }
+
+    // actualizar variable compartida
+    pthread_mutex_lock(&mutex);
+    ocurrencias += ocurrenciasLocal;
+    pthread_mutex_unlock(&mutex);
+
+    return NULL;   
+}
---
+++ I404 - LIDI/P2/ej2_seq.c
@@ -0,0 +1,73 @@
+// Dado un vector de N elementos, contar las ocurrencias de un numero dado
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Cabecera para dwall time
+#include <sys/time.h>
+
+// ******************************* DWALL TIME *********************** //
+
+//Para calcular tiempo
+double dwalltime(){
+        double sec;
+        struct timeval tv;
+
+        gettimeofday(&tv,NULL);
+        sec = tv.tv_sec + tv.tv_usec/1000000.0;
+        return sec;
+}
+
+// ----------------------------------------------------------------------
+
+// variables globales
+static int *v;
+static const int X = 10;
+static int ocurrencias = 0;
+
+// prototipos de función
+static void init_vector(int* v, int N, int max);
+
+int main(int argc,char*argv[]){
+
+    // variables
+    int N, i;
+    double timetick;
+
+    // lectura de argumentos
+    if ((argc != 2) || ((N = atoi(argv[1])) <= 0) ){
+        printf("\nError: hay que especificar N (tamaño del vector)\n");
+        exit(1);
+    }
+
+    // reservar memoria
+    v = (int*) malloc(sizeof(int)*N);
+
+    // rellenar vector
+    init_vector(v, N, 2*X);
+
+    // marcar inicio de tiempo
+    timetick = dwalltime();
+
+    // resolver
+    for (i=0; i<N; i++){
+        if (v[i] == X) ocurrencias++;
+    }
+
+    // marcamos fin de tiempo
+    printf("Ocurrrencias de %d en vector: %d. Tiempo en segundos %f\n",X,ocurrencias, dwalltime() - timetick);
+
+    return 0;
+}
+
+static void init_vector(int* v, int N, int max){
+    int i;
+
+    // nueva seed
+    srand(time(NULL));
+
+    for (i=0; i<N; i++){
+        // asignar numero entre 0 y 2X
+        v[i] = rand() % max;
+    }
+}
