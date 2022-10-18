afa9032e76b1c0a8625b1f12fac3b34ee87aa1f8
Sergio Calderón <66924320+sergiocarp10@users.noreply.github.com>
2022-10-18 03:00:20 -0300

    corrección: est completa

--- I162 - Big Data/TP4 - Spark I/ej5_estaciones.py
+++ I162 - Big Data/TP4 - Spark I/ej5_estaciones.py
@@ -33,14 +33,14 @@ print(promedios)
 
 ## Parte 2:
 # minimos y maximos: de esta forma es 4 veces más rápido
-part2 = estaciones.map(lambda e: (e[2], e[2], e[3], e[3], e[4], e[4]) )
+part2 = estaciones.map(lambda e: (e, e, e, e, e, e) )
 part2 = part2.reduce(lambda e1, e2: 
-                          (e1[0] if e1[0] < e2[0] else e2[0], 
-                           e1[1] if e1[1] > e2[1] else e2[1],
-                           e1[2] if e1[2] > e2[2] else e2[2],
-                           e1[3] if e1[3] < e2[3] else e2[3],
-                           e1[4] if e1[4] > e2[4] else e2[4],
-                           e1[5] if e1[5] < e2[5] else e2[5]) 
+                          (e1[0] if e1[0][2] < e2[0][2] else e2[0], 
+                           e1[1] if e1[1][2] > e2[1][2] else e2[1],
+                           e1[2] if e1[2][3] > e2[2][3] else e2[2],
+                           e1[3] if e1[3][3] < e2[3][3] else e2[3],
+                           e1[4] if e1[4][4] > e2[4][4] else e2[4],
+                           e1[5] if e1[5][4] < e2[5][4] else e2[5]) 
                           )
 
 print(part2)
