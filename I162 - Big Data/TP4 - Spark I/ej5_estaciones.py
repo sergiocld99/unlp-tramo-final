737ee0dec9dcc95ce45991c088b1bfa15107c0c5
Sergio Calderón <66924320+sergiocarp10@users.noreply.github.com>
2022-10-18 21:09:45 -0300

    ej2 (p5) con 6 transformaciones

--- I162 - Big Data/TP4 - Spark I/ej5_estaciones.py
+++ I162 - Big Data/TP4 - Spark I/ej5_estaciones.py
@@ -14,16 +14,29 @@ inputDir = root_path + "EstacionesMeteorologicas/"
 # ------------------- transformaciones ----------------------
 
 # <ID_Estación, fecha_registro, temperatura, humedad, precipitación>
+# norte: en grados centigrados y mm
+# sur: en grados fahrenheit tc = (tf - 32) * 1.8, y cm
 
 def splitter(linea):
   return linea.split('\t')
 
+def celsius(fahrenheit):
+  return 1.8 * (int(fahrenheit) - 32)
+
+def cm(mm):
+  return 10 * int(mm)
+
 # ------------------- carga de archivos ----------------------
 
-estaciones = sc.textFile(inputDir)
-estaciones = estaciones.map(splitter)
-estaciones = estaciones.map(lambda e: (e[0], e[1], int(e[2]), int(e[3]), int(e[4])) )
-print(estaciones.first())
+norte = sc.textFile(inputDir + "Norte").map(splitter)
+sur = sc.textFile(inputDir + "Sur").map(splitter)
+
+# conversion a enteros y a sistema internacional
+norte = norte.map(lambda e: (e[0], e[1], int(e[2]), int(e[3]), int(e[4])) )
+sur = sur.map(lambda e: (e[0], e[1], celsius(e[2]), int(e[3]), cm(e[4]) ) )
+
+# juntar datasets
+estaciones = norte.union(sur)
 
 ## Parte 1: promedio de temperatura, humedad y precipitacion
 part1 = estaciones.map(lambda e: (e[2], e[3], e[4], 1) )
@@ -33,14 +46,14 @@ print(promedios)
 
 ## Parte 2:
 # minimos y maximos: de esta forma es 4 veces más rápido
-part2 = estaciones.map(lambda e: (e, e, e, e, e, e) )
+part2 = estaciones.map(lambda e: (e[2], e[2], e[3], e[3], e[4], e[4]) )
 part2 = part2.reduce(lambda e1, e2: 
-                          (e1[0] if e1[0][2] < e2[0][2] else e2[0], 
-                           e1[1] if e1[1][2] > e2[1][2] else e2[1],
-                           e1[2] if e1[2][3] > e2[2][3] else e2[2],
-                           e1[3] if e1[3][3] < e2[3][3] else e2[3],
-                           e1[4] if e1[4][4] > e2[4][4] else e2[4],
-                           e1[5] if e1[5][4] < e2[5][4] else e2[5]) 
+                          (e1[0] if e1[0] < e2[0] else e2[0], 
+                           e1[1] if e1[1] > e2[1] else e2[1],
+                           e1[2] if e1[2] > e2[2] else e2[2],
+                           e1[3] if e1[3] < e2[3] else e2[3],
+                           e1[4] if e1[4] > e2[4] else e2[4],
+                           e1[5] if e1[5] < e2[5] else e2[5]) 
                           )
 
 print(part2)
