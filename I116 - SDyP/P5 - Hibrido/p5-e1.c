e9c2b7238255bb956d3f21455defc01bd7e1dc30
Sergio Calderón <66924320+sergiocarp10@users.noreply.github.com>
2023-05-09 09:47:53 -0300

    mpi + omp

---
+++ I116 - SDyP/P5 - Hibrido/p5-e1.c
@@ -0,0 +1,139 @@
+#include <stdio.h>
+#include <stdlib.h>     // atoi
+#include "tiempo.h"
+#include <mpi.h>
+#include <omp.h>
+
+// NO EXISTEN VARIABLES COMPARTIDAS EN MPI
+void getParams(int argc, char* argv[], int *N, int *T);
+void p0(int N, int cp);
+
+// Funciones comunes
+void resolverOMP(double *A, double *B, double *C, int filas, int cols, int N);
+
+// Funciones de verificacion
+void saludar();
+
+// programa principal
+int main(int argc, char* argv[]){
+
+    // siempre primero esto!!
+    MPI_Init(&argc, &argv);
+
+    // variables locales
+    int id;
+    int cantProcesos;
+    int N, T;
+    
+    getParams(argc, argv, &N, &T);
+
+    // obtener id y cant procesos
+    MPI_Comm_rank(MPI_COMM_WORLD, &id);
+    MPI_Comm_size(MPI_COMM_WORLD, &cantProcesos);
+
+    if (N < 1 || T < 1 || N % T != 0) {
+        printf("Formato: mpirun --bind-to none -np CP archivo N T\n");
+        MPI_Finalize();
+        return 1;
+    }
+    
+    // establecer cantidad de hilos por rank
+    omp_set_num_threads(T);
+
+    if (id == 0) p0(N, cantProcesos);
+
+    // siempre por ultimo esto!!
+    MPI_Finalize();
+
+    return 0;
+}
+
+// funcion que ejecuta el proceso con id=0
+void p0(int N, int cp){
+    double *A, *B, *C;
+    double *AL;
+    int i,j;
+    int filasRep = N / cp;
+    int errores = 0;
+    
+    // reservamos memoria
+    A = (double*) malloc(N*N*sizeof(double));
+    B = (double*) malloc(N*N*sizeof(double));
+    C = (double*) malloc(N*N*sizeof(double));
+    
+    // creamos las matrices
+    for (i=0; i<N; i++){
+    	for (j=0; j<N; j++){
+    		A[i*N+j] = 1;
+    		B[i+j*N] = 1;
+    		C[i*N+j] = 0;
+    	}
+    }
+    
+    // printf("Tenemos %d procesos\n", cp);
+    saludar();
+    double t0 = dwalltime();
+    
+    // repartimos por conjuntos de filas
+    MPI_Scatter(A, filasRep*N, MPI_DOUBLE, A, filasRep*N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(B, N*N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    
+    // resolver mi parte...
+    resolverOMP(A,B,C,filasRep,N,N);
+    
+    // juntar los resultados de cada uno
+    MPI_Gather(C, filasRep*N, MPI_DOUBLE, C, filasRep*N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    
+    double t1 = dwalltime();
+   	
+    // verificacion
+    for (i=0; i<N; i++){
+    	for (j=0; j<N; j++){
+    		if (C[i*N+j] != N) errores++;
+    	}
+    }
+    
+    printf("Tiempo: %f segundos, %d errores\n", t1-t0, errores);
+   	
+    // liberamos memoria
+    free(A);
+		free(B);
+		free(C);
+}
+
+// funciones comunes
+void resolverOMP(double *A, double *B, double *C, int filas, int cols, int N){
+		int i,j,k;
+	
+		// paralelizacion por hilos
+		// Se recorren pocas filas de A y todas las columnas de B
+		
+		#pragma omp parallel for private(i,j,k) shared(A,B,C,filas,cols,N) default(none)
+		for (i=0; i<filas; i++){
+			for (j=0; j<cols; j++){
+				for (k=0; k<N; k++){
+					// C[i,j] += A[i,k] * B[k,j]
+					C[i*N+j] += A[i*N+k] * B[k+j*N];
+				}
+			}
+		}
+}
+
+void saludar(){
+		#pragma omp parallel 
+		{
+			printf("Hola desde hilo %d\n", omp_get_thread_num());
+		}
+}
+
+// control de argumentos
+void getParams(int argc, char* argv[], int *N, int *T){
+    
+    if (argc < 3){
+    	*N = 0;
+    	*T = 0;
+    } else {
+    	*N = atoi(argv[1]);
+    	*T = atoi(argv[2]);
+    }
+}
